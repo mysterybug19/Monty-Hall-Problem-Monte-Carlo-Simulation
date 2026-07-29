c74739140e2096e9f1b8ca076d7565bd96f030b6
mysterybug19 <cristirusisoru@gmail.com>
2026-07-29 22:39:10 +0300

    Enhance Monty Hall game logic and UI
    Added player door selection and game rules explanation.

--- start.py
+++ start.py
@@ -10,14 +10,14 @@ if "page" not in st.session_state:
     st.session_state.page = "intro"
 if "N" not in st.session_state:
     st.session_state.N = 0
+if "player_door" not in st.session.state:
+    st.session_state.player_door=0
     
 #Intro
 if st.session_state.page == "intro":
     st.title("Simulare Monty Hall")
     st.divider()
-    st.markdown("""
-    Platforma ce explică acestă problemă clasică de statistică și efectuază simulări pe baza acestor rezultate.
-    """)
+    st.markdown("""Platforma ce explică acestă problemă clasică de statistică și efectuază simulări pe baza acestor rezultate.""")
 
     if st.button("Jocul", use_container_width=True):
         st.session_state.page = "game"
@@ -35,6 +35,23 @@ if st.session_state.page == "intro":
 if st.session_state.page == "game":
     st.title("Jocul")
     st.divider()
+    st.markdown("""Regulile jocului: Regulile Problemei lui Monty Hall presupun trei uși închise, o mașină ascunsă și două capre. Jucătorul alege o ușă, prezentatorul deschide altă ușă cu o capră, iar jucătorul decide dacă păstrează sau schimbă ușa.""")
+    col1, col2, col3= st.columns(3)
+    with col1:
+        if st.button("Ușa 1", use_container_width=True):
+            st.session_state.player_door=1
+            st.session_state.page = "game1"
+            st.rerun()
+    with col2:
+        if st.button("Ușa 2", use_container_width=True):
+            st.session_state.player_door=2
+            st.session_state.page = "game1"
+            st.rerun()
+    with col3:
+        if st.button("Ușa 3", use_container_width=True):
+            st.session_state.player_door=3
+            st.session_state.page = "game1"
+            st.rerun()
     if st.button("Înapoi"):
         st.session_state.page = "intro"
         st.rerun()
@@ -46,12 +63,53 @@ if st.session_state.page == "explenation":
         st.session_state.page = "intro"
         st.rerun()
 
+if st.session_state.page == "game1":
+    st.title("Jocul")
+    st.divider()
+    doors=[1,2,3]
+    car_door = random.choice(doors)
+    player_door=st.session_state.player_door
+    possible = [d for d in doors if d != player_door and d != car_door]
+    host_door = random.choice(possible_door)
+    col1, col2= st.columns(2)
+    with col1:
+        if st.button("Păstrez", use_container_width=True):
+            if player_door==car_door:
+                st.session_state.page = "winner"
+                st.rerun()
+            else:
+                st.session_state.page = "losser"
+                st.rerun()
+    with col2:
+        if st.button("Schimb", use_container_width=True):
+            if player_door==car_door:
+                st.session_state.page = "losser"
+                st.rerun()
+            else:
+                st.session_state.page = "winner"
+                st.rerun()
+    if st.button("Înapoi"):
+        st.session_state.page = "game"
+        st.rerun()
+#Winner
+if st.session_state.page == "winner":
+    st.title("Ai câiștigat")
+    st.ballons()
+    if st.button("Înapoi"):
+        st.session_state.page = "intro"
+        st.rerun()
+if st.session_state.page == "loser":
+    st.title("Ai pierdut")
+    if st.button("Înapoi"):
+        st.session_state.page = "intro"
+        st.rerun()
+    
 #Simulations
 if st.session_state.page == "simulation_nr":
     st.title("Simulare Monty Hall")
     st.header("Alege numărul de simulări")
-    st.session_state.N=st.number_input("Număr Simulări (1-10e8)", min_value=1, max_value=100000000)
-    col1, col2 = st.columns(2)
+    st.session_state.N=st.number_input("Număr Simulări (1-10e7)", min_value=1, max_value=10000000)
+    col1, col2= st.columns(2)
     with col1:
         if st.button("Înapoi", use_container_width=True):
             st.session_state.page = "intro"
@@ -86,7 +144,7 @@ if st.session_state.page == "simulation":
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=x,y=stay_plot,mode="lines",name="Stay"))
     fig.add_trace(go.Scatter(x=x,y=switch_plot,mode="lines",name="Switch"))
-    fig.update_layout(title="Monty Hall Convergence",xaxis_title="Number of simulations",yaxis_title="Probability",)
+    fig.update_layout(title="Convergența Monty Hall",xaxis_title="Number of simulations",yaxis_title="Probability",)
     fig.update_xaxes(type="log")
     fig.add_hline(y=1/3, line_dash="dash")
     fig.add_hline(y=2/3, line_dash="dash")
