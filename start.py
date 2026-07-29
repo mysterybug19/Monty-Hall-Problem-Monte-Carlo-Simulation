9ed6754ac9182c7104c00fef2f39a3ffa44f6e17
mysterybug19 <cristirusisoru@gmail.com>
2026-07-29 18:04:41 +0300

    Refactor Monty Hall simulation logic and UI

--- start.py
+++ start.py
@@ -57,9 +57,6 @@ if st.session_state.page == "simulation_nr":
     st.title("Simulare Monty Hall")
     st.header("Alege numărul de simulări")
     st.session_state.N=st.number_input("Număr Simulări (1-10e6)", min_value=1, max_value=1000000)
-    st.session_state.idx  = 0
-    st.session_state.stay_wins = 0
-    st.session_state.switch_wins = 0
     col1, col2 = st.columns(2)
     with col1:
         if st.button("Înapoi", use_container_width=True):
@@ -76,37 +73,28 @@ if st.session_state.page == "simulation":
     st.divider()
     doors=[1,2,3]
     N=st.session_state.N
-    idx=st.session_state.idx
-    if idx<N:
+    stay_wins=0
+    switch_wins=0
+    idx=0
+    while idx<N:
         car = random.choice(doors)
         player = random.choice(doors)
         possible = [d for d in doors if d != player and d != car]
         host = random.choice(possible)
         switch = [d for d in doors if d != player and d != host][0]
         if player == car:
-            st.session_state.stay_wins += 1
+            stay_wins += 1
         if switch == car:
-            st.session_state.switch_wins += 1
-        stay_wins=st.session_state.stay_wins
-        switch_wins=st.session_state.switch_wins
+            switch_wins += 1
         idx+=1
-        st.session_state.idx=idx
-        st.markdown("Victorii dacă nu schimb")
-        st.progress(float(stay_wins)/float(idx))
-        st.caption(str(float(stay_wins)/float(idx)))
-        st.markdown("Victorii dacă schimb")
-        st.progress(float(switch_wins)/float(idx))
-        st.caption(str(float(switch_wins)/float(idx)))
-        st.rerun()
-    else:
-        stay_wins=st.session_state.stay_wins
-        switch_wins=st.session_state.switch_wins
-        st.markdown("Victorii dacă nu schimb")
-        st.progress(float(stay_wins)/float(idx))
-        st.caption(str(float(stay_wins)/float(idx)))
-        st.markdown("Victorii dacă schimb")
-        st.progress(float(switch_wins)/float(idx))
-        st.caption(str(float(switch_wins)/float(idx)))
+    stay_pr=float(stay_wins/N*100)
+    switch_pr=float(switch_wins/N*100)
+    st.markdown("Victorii dacă nu schimb")
+    st.progress(stay_pr/100,00)
+    st.caption(f"{stay:.1f}%")
+    st.markdown("Victorii dacă schimb")
+    st.progress(switch_pr/100,00)
+    st.caption(f"{switch:.1f}%")
 
     if st.button("Back"):
         st.session_state.page = "intro"
